6e095ffa41ae7e9bc4676f9d91126bc850cc6bab
djuna <djuna@mit.edu>
2023-12-09 19:22:50 -0500

    updating with save mean proj for each image, also making preprocessing optional

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from os import path
 
 from confocalQuant.segmentation import load_3D, int_to_float, run_med_filter, bgrnd_subtract, get_anisotropy, do_inference,  sigmoid, gamma_correct_image
 from confocalQuant.quantification import get_all_expectations
+from confocalQuant.image import save_mean_proj
 
 import argparse
 from distutils.util import strtobool
@@ -31,7 +32,7 @@ def get_czi_files(directory): # this function is chatGPT3
     files = [file for file in os.listdir(directory) if file.endswith(".czi")]
     return sorted(files)
 
-def process_image(folder, im_path, ID, model, channels, y_channel, kernel, per_channel_subtraction_vals, diameter, inf_channels, min_size, Ncells, NZi, start_Y, start_zi, xi_per_job, yi_per_job, Njobs, gamma_dict, lower_thresh_dict, upper_thresh_dict):
+def process_image(folder, im_path, ID, model, channels, y_channel, kernel, per_channel_subtraction_vals, diameter, inf_channels, min_size, Ncells, NZi, start_Y, start_zi, xi_per_job, yi_per_job, Njobs, gamma_dict, lower_thresh_dict, upper_thresh_dict, outdir, preprocess):
     mmap_1 = path.join(folder, 'mat.npy')
 
     if not path.exists(mmap_1):
@@ -44,6 +45,7 @@ def process_image(folder, im_path, ID, model, channels, y_channel, kernel, per_c
     all_Y = np.lib.format.open_memmap(path.join(folder, 'Y.npy'), shape=(Ncells, len(channels)+len(y_channel)+3), dtype=float, mode=mode)
     Ncells_per_job = np.lib.format.open_memmap(path.join(folder, 'Ncells_per_job.npy'), shape=(Njobs,1), dtype=int, mode=mode)
     Nzi_per_job = np.lib.format.open_memmap(path.join(folder, 'Nzi_per_job.npy'), shape=(Njobs,1), dtype=int, mode=mode)
+    randomID_per_job = np.lib.format.open_memmap(path.join(folder, 'randomID_per_job.npy'), shape=(Njobs,1), dtype=int, mode=mode)
 
     # load image with channels from above
     print('loading image')
@@ -54,25 +56,38 @@ def process_image(folder, im_path, ID, model, channels, y_channel, kernel, per_c
     out_float = int_to_float(out)
 
     # run med filter to remove noise
-    print('runing median filter')
-    out_med = run_med_filter(out_float, kernel = kernel)
+    if preprocess:
+        print('preprocess is True')
+        print('runing median filter')
+        if kernel!=1:
+            out_med = run_med_filter(out_float, kernel = kernel)
+        else:
+            out_med = out_float
 
-    # run background subtraction 
-    print('running background subtraction')
-    out_float_subtract = bgrnd_subtract(out_med, np.array(per_channel_subtraction_vals))
+        # run background subtraction 
+        print('running background subtraction')
+        out_float_subtract = bgrnd_subtract(out_med, np.array(per_channel_subtraction_vals))
 
-    # run upper thresholding
-    print('upper threshold')
-    
-    out_float_subtract_correct = gamma_correct_image(out_float_subtract, gamma_dict, lower_thresh_dict, upper_thresh_dict)
+        # run upper thresholding
+        print('upper threshold')
 
-    # run inference
-    print('doing inference')
-    anisotropy = get_anisotropy(img)
-    print('Anisotropy: ' + str(anisotropy))
-    
-    masks, flows = do_inference(out_float_subtract_correct, do_3D=True, model=model, anisotropy=anisotropy, diameter=diameter, channels=inf_channels, channel_axis=3, z_axis=0, min_size=min_size, normalize = False)
-        
+        out_float_subtract_correct = gamma_correct_image(out_float_subtract, gamma_dict, lower_thresh_dict, upper_thresh_dict)
+
+        # run inference
+        print('doing inference')
+        anisotropy = get_anisotropy(img)
+        print('Anisotropy: ' + str(anisotropy))
+
+        masks, flows = do_inference(out_float_subtract_correct, do_3D=True, model=model, anisotropy=anisotropy, diameter=diameter, channels=inf_channels, channel_axis=3, z_axis=0, min_size=min_size, normalize = False)
+
+    else:
+        print('preprocess is False')
+        # run inference
+        print('doing inference')
+        anisotropy = get_anisotropy(img)
+        print('Anisotropy: ' + str(anisotropy))
+        masks, flows = do_inference(out_float, do_3D=True, model=model, anisotropy=anisotropy, diameter=diameter, channels=inf_channels, channel_axis=3, z_axis=0, min_size=min_size, normalize = False)
+
     # get expectations
     print('computing expectations')
     probs = sigmoid(flows[2])
@@ -88,6 +103,14 @@ def process_image(folder, im_path, ID, model, channels, y_channel, kernel, per_c
     # add image ID
     Y = np.concatenate((Y, np.repeat(ID, Y.shape[0]).reshape(-1,1)), axis=1)
     
+    # save projection 
+    print('saving projection')
+    np.random.seed(ID)
+    if not path.exists(outdir):
+        os.makedirs(outdir)
+    randomID = save_mean_proj(out_float, masks, outdir)
+    print(random_ID)
+    
     # save mat, masks, and Y_filtered
     print('saving..')
     actual_NZi = out_float.shape[0]
@@ -100,7 +123,8 @@ def process_image(folder, im_path, ID, model, channels, y_channel, kernel, per_c
     all_Y[start_Y:end_Y] = Y
     Ncells_per_job[ID] = actual_Ncells
     Nzi_per_job[ID] = actual_NZi
-
+    randomID_per_job[ID] = randomID
+    
     print('done')
     
 if __name__ == '__main__':
@@ -125,6 +149,8 @@ if __name__ == '__main__':
     parser.add_argument('--gamma_dict', type=parse_dict, required=True)
     parser.add_argument('--lower_thresh_dict', type=parse_dict, required=True)
     parser.add_argument('--upper_thresh_dict', type=parse_dict, required=True)
+    parser.add_argument('--outdir', type=str, required=True)
+    parser.add_argument('--preprocess', action='store_true', help='Enable preprocessing', default=False)
 
     args = parser.parse_args()
     cells_per_job = args.cells_per_job
@@ -143,7 +169,7 @@ if __name__ == '__main__':
     print(im_path)
     
     print('starting processing')
-    process_image(args.folder, im_path, ID, model, args.channels, args.y_channel, args.kernel, args.bgrnd_subtraction_vals, args.diameter, args.inf_channels, args.min_size, args.Ncells, args.NZi, start_Y, start_zi, args.xi_per_job, args.yi_per_job, args.Njobs, args.gamma_dict, args.lower_thresh_dict, args.upper_thresh_dict)
+    process_image(args.folder, im_path, ID, model, args.channels, args.y_channel, args.kernel, args.bgrnd_subtraction_vals, args.diameter, args.inf_channels, args.min_size, args.Ncells, args.NZi, start_Y, start_zi, args.xi_per_job, args.yi_per_job, args.Njobs, args.gamma_dict, args.lower_thresh_dict, args.upper_thresh_dict, args.outdir, args.preprocess)
     
     
     
