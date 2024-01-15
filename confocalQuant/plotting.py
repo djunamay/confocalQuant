68a2e453f648d87d0842a622e6ca91e1130dfc8a
djuna <djuna@mit.edu>
2024-01-15 12:56:27 -0500

    adding docstrings

--- confocalQuant/plotting.py
+++ confocalQuant/plotting.py
@@ -10,6 +10,142 @@ from aicsimageio import AICSImage
 from .segmentation import bgrnd_subtract, gamma_correct_image, extract_channels
 import os 
 
+def plot_hist(path, channel, nbins, scale_log, alpha, color, density):
+    """
+    Plot histogram of pixel intensities from a specified channel in an AICSImage.
+
+    Parameters:
+    - path (str): Path to the AICSImage file.
+    - channel (int): Index of the channel to extract pixel intensities.
+    - nbins (int): Number of bins in the histogram.
+    - scale_log (bool): Whether to scale the histogram in log scale.
+    - alpha (float): Transparency of the histogram bars.
+    - color (str): Color of the histogram bars.
+    - density (bool): Whether to normalize the histogram to form a probability density.
+
+    Returns:
+    - None
+    """
+    img = AICSImage(path)
+    T = img.data[0,channel,:,:,:].ravel()
+    T[T==0] = 1
+    plt.hist(np.log(int_to_float(T)),nbins, alpha=alpha, color=color, density=density)
+    None
+  
+def plot_treatments(df, x, line, colors, lognormal, binwidth,lab_height, grp='well', size=(20,7)):
+    """
+    Plot treatments comparison for a specific line in the dataframe.
+
+    Parameters:
+    - df (pd.DataFrame): DataFrame containing the data.
+    - x (str): Variable to be plotted.
+    - line (str): Specific line for which treatments are compared.
+    - colors (list): List of colors for treatments.
+    - lognormal (bool): Whether to use log scale for the variable.
+    - binwidth (float): Width of bins in the histogram.
+    - lab_height (float): Height of the p-value text annotation.
+    - grp (str): Grouping variable for boxplot. Default is 'well'.
+    - size (tuple): Size of the plot figure. Default is (20, 7).
+
+    Returns:
+    - None
+    """
+    sns.set(rc={'figure.figsize':size})
+    sns.set_style("whitegrid")
+    df_temp = df[df['line']==line]
+
+    if lognormal:
+        df_temp[x] = np.log(df_temp[x])
+    
+    f, ax = plt.subplots(3,len(np.unique(df['treatment']))-1, sharex=True, gridspec_kw={"height_ratios": (1,.25,.5)})
+
+    for i,t in enumerate(np.unique(df['treatment'])[:-1]):
+        index = (df_temp['treatment']=='vehicle') | (df_temp['treatment']==t)
+        
+        # compute pvalue 
+        text = compute_nested_anova(df_temp[index], x, 'treatment', grp)
+        sns.histplot(data=df_temp[index], x=x, ax=ax[0,i], hue='treatment', binwidth=binwidth, element="step", common_norm=False, stat='density', palette = colors, order=('vehicle', t))
+        
+        ax[0,i].text(x=np.mean(df_temp[index][x]), y=lab_height, s=text, fontsize=12, color='black')
+        
+        sns.boxplot(data=df_temp[index], x=x, y='treatment',orient="h", ax=ax[1,i], width=.5, dodge=True, palette = colors)
+        sns.boxplot(data=df_temp[index], x=x, y=grp,orient="h", ax=ax[2,i], width=.8, dodge=False, hue = 'treatment', palette = colors)
+        ax[2,i].get_legend().remove()
+        ax[1,i].axes.get_yaxis().set_visible(False)
+
+
+def plot_lines(df, x, treatment, colors, lognormal, binwidth, grp='well'):
+    """
+    Plot lines comparison for a specific treatment in the dataframe.
+
+    Parameters:
+    - df (pd.DataFrame): DataFrame containing the data.
+    - x (str): Variable to be plotted.
+    - treatment (str): Specific treatment for which lines are compared.
+    - colors (list): List of colors for lines.
+    - lognormal (bool): Whether to use log scale for the variable.
+    - binwidth (float): Width of bins in the histogram.
+    - grp (str): Grouping variable for boxplot. Default is 'well'.
+
+    Returns:
+    - None
+    """
+    sns.set(rc={'figure.figsize':(6,12)})
+    sns.set_style("whitegrid")
+    df_temp = df[df['treatment']==treatment]
+
+    if lognormal:
+        df_temp[x] = np.log(df_temp[x])
+        
+    f, ax = plt.subplots(6, sharex=True, gridspec_kw={"height_ratios": (1,.5,1,1,.5,1)})
+
+    index = df_temp['line']!='G2'
+    
+    # compute pvalue 
+    text0 = compute_nested_anova(df_temp[index], x, 'line', grp)
+    sns.histplot(data=df_temp[index], x=x, ax=ax[0], hue='line', binwidth=binwidth, element="step", common_norm=False, stat='density', palette = colors)
+    ax[0].text(x=np.mean(df_temp[index][x])+2, y=.9, s=text0, fontsize=12, color='black')
+    sns.boxplot(data=df_temp[index], x=x, y='line',orient="h", ax=ax[1], width=.5, dodge=True, palette = colors)
+    sns.boxplot(data=df_temp[index], x=x, y=grp,orient="h", ax=ax[2], width=.8, dodge=False, hue = 'line', palette = colors)
+
+    index = df_temp['line']!='Y622'
+    
+    # compute pvalue 
+    text1 = compute_nested_anova(df_temp[index], x, 'line', grp)
+    sns.histplot(data=df_temp[index], x=x, ax=ax[3], hue='line', binwidth=binwidth, element="step", common_norm=False, stat='density', palette = colors)
+    ax[3].text(x=np.mean(df_temp[index][x])+2, y=.9, s=text1, fontsize=12, color='black')
+    sns.boxplot(data=df_temp[index], x=x, y='line',orient="h", ax=ax[4], width=.5, dodge=True, palette = colors)
+    sns.boxplot(data=df_temp[index], x=x, y=grp,orient="h", ax=ax[5], width=.8, dodge=False, hue = 'line', palette = colors)
+    
+    for i in range(6):
+        ax[i].legend(loc='upper left', bbox_to_anchor=(1, 1))
+    
+
+def plot_scatter(condition, x, y, xlab, ylab):
+    """
+    Scatter plot grouped by a condition.
+
+    Parameters:
+    - condition (np.ndarray): Array containing the grouping condition.
+    - x (np.ndarray): Values for the x-axis.
+    - y (np.ndarray): Values for the y-axis.
+    - xlab (str): Label for the x-axis.
+    - ylab (str): Label for the y-axis.
+
+    Returns:
+    - None
+    """
+    for i in np.unique(condition):
+        index = condition==i
+        plt.scatter(x[index], y[index], label=i)
+        plt.legend()
+        plt.xlabel(xlab)
+        plt.ylabel(ylab)
+        plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+
+
+######
+        
 def extract_sbatch_parameters(file_path):
     """
     Extract parameters from a Slurm sbatch script file and return them as a dictionary.
