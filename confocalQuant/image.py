0944d10ccefd5191f2401efd8e2fdd185615e1da
djuna <djuna@mit.edu>
2024-01-15 12:02:16 -0500

    adding docstring and removing prev save proj function

--- confocalQuant/image.py
+++ confocalQuant/image.py
@@ -5,7 +5,18 @@ from PIL import Image
 from skimage.segmentation import find_boundaries
 
 def save_mean_proj(mat_sele, mask_sele, outdir):
-    
+    """
+    Calculate the mean projection of a 3D matrix, perform gamma correction,
+    extract specific channels, apply a binary mask specifying cell boundaries, and save the resulting image.
+
+    Parameters:
+    - mat_sele (np.ndarray): 3D matrix for mean projection.
+    - mask_sele (np.ndarray): Binary mask for channel extraction.
+    - outdir (str): Output directory to save the processed image.
+
+    Returns:
+    - int: Randomly generated ID for the saved image.
+    """
     mat_proj = np.mean(mat_sele, axis=(0))
     gamma_dict={0: 1, 1: 1, 2: 1, 3:1}
     lower_dict={0: 0, 1: 0, 2: 0, 3:0}
@@ -31,37 +42,3 @@ def save_mean_proj(mat_sele, mask_sele, outdir):
 
     return random_ID
 
-# def save_mean_proj(ID, zi_per_job, Nzi, mat, masks, Y_extracted, Y_filtered, id_d, outdir):
-    
-#     # add start and end info
-#     mat_sele, mask_sele = get_id_data(ID, zi_per_job, Nzi, mat, masks)
-#     mask_sele2 = hide_masks(Y_extracted, ID, Y_filtered, mask_sele)
-
-#     mat_proj = np.mean(mat_sele, axis=(0))
-#     gamma_dict={0: .5, 1: 1, 2: .5}
-#     lower_dict={0: 0, 1: 0, 2: 0}
-#     upper_dict={0: 100, 1: 99.9, 2: 100}
-
-#     mat_g = gamma_correct_image(mat_proj, gamma_dict, lower_dict, upper_dict, is_4D=False)
-#     show = extract_channels([1], mat_g, is_4D=False)
-
-#     gamma_dict={0: .5, 1: 1, 2: .5}
-#     lower_dict={0: 0, 1: 0, 2: 0}
-#     upper_dict={0: 100, 1: 99.9, 2: 100}
-
-#     mat_g = gamma_correct_image(mat_proj, gamma_dict, lower_dict, upper_dict, is_4D=False)
-#     show_copy = extract_channels([1], mat_g, is_4D=False)
-
-
-#     index = mask_sele2>0
-#     index = np.max(index, axis=(0))
-#     index = index.astype(int)
-#     index = find_boundaries(index, mode = 'outer', background = 0) 
-#     for i in range(show.shape[-1]):
-#         show_copy[:,:,i][index]=1
-
-#     temp = np.zeros((1024,10,3))
-#     test = np.concatenate((show, temp, show_copy), axis = (1))
-#     image = Image.fromarray(float_to_int(test))
-#     path = outdir +str(id_d[ID])+'.png'
-#     image.save(path)
